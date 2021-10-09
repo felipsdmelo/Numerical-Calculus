936e6c0b81ab6e6d2c5a46c599a16611f45bfc94
Felipe Melo <felipediasdemelo1@gmail.com>
2021-10-09 02:26:32 -0300

    Add Runge-Kutta's fourth order method

--- Numerical Solution for ODE/runge_kutta_methods.py
+++ Numerical Solution for ODE/runge_kutta_methods.py
@@ -1,8 +1,3 @@
-from math import *
-
-def f(x, y) :
-    return sin(x * y)
-
 def runge_kutta_third_order(x0, y0, xf, h, f) :
     '''
     Approximates the value of an ODE using Runge-Kutta's
@@ -27,6 +22,36 @@ def runge_kutta_third_order(x0, y0, xf, h, f) :
         k2 = h * f(x + h / 2, y + k1 / 2)
         k3 = h * f(x + 3 * h / 4, y + 3 * k2 / 4)
         yf = y + 1 / 9 * (2 * k1 + 3 * k2 + 4 * k3)
+        x += h
+        y = yf
+    return yf
+
+
+def runge_kutta_fourth_order(x0, y0, xf, h, f) :
+    '''
+    Approximates the value of an ODE using Runge-Kutta's
+    fourth order method
+
+    Keyword arguments:
+    x0 - initial value of x
+    y0 - initial value of y
+    xf - final value of x
+    h - size of the intervals (step)
+    f - function of x and y (usually the right side of the equation)
+
+    Returns:
+    An estimate for the ODE
+    '''
+    n = int((xf - x0) / h) # number of iterations
+    x = x0
+    y = y0
+    yf = 0 # avoids local variable reference error
+    for i in range(n) :
+        k1 = h * f(x, y)
+        k2 = h * f(x + h / 2, y + k1 / 2)
+        k3 = h * f(x + h / 2, y + k2 / 2)
+        k4 = h * f(x + h, y + k3)
+        yf = y + 1 / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
         x += h
         y = yf
     return yf
